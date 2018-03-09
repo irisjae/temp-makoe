e39d61e544a4fe601094deb78db7f2457e06979c
mumenrider <brian.aquinde@outlook.com>
2018-03-09 01:14:07 +0000

    checkpoint

--- go/serve.js
+++ go/serve.js
@@ -26,6 +26,24 @@ require ('koa-qs') (new (require ('koa')) ())
 	.use (require ('koa-bodyparser') ())
 	.use (require ('koa-json') ())
 	.use (require ('koa-static') (__dirname, { extensions: ['html'] }))
+	.use (require ('koa-router') () .post ('/send-apply', (ctx, next) => {
+		return Promise .resolve ()
+		.then (x => ctx .request .body)
+		.then (x => {
+			var uuid = require ('uuid/v4') ()
+			var path = require ('path') .join (__dirname, 'applications', uuid)
+			return require ('fs-extra') .outputJSON (path, x)
+		})
+		.then (x => ({ ok: true }))
+		.catch (x => {
+			console .error (x)
+			return { error: 'An unexpected error occured' }
+		})
+		.then (x => {
+			ctx .body = x
+		})
+		.then (next)
+	}) .routes ())
 	
 	.listen (port);
 
