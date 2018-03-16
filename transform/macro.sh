d40415ea7ba5ef133ec8a8287efca22d608572af
mumenrider <brian.aquinde@outlook.com>
2018-03-16 10:18:15 +0000

    rebuild

---
+++ transform/macro.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+cd "$(dirname $0)"
+
+cd ..
+
+next="$1"
+
+rm src/*
+cp scenes/base.html src/base.html
+find scenes -name "$next.*" | while read file; do
+	cp "$file" "src/next.${file##*.}"
+done
+
+./transform/page.sh > dist/next.html
+
+mv "assets/liquefied" "assets/$next"
+cat dist/next.html | sed s,assets/liquefied,assets/$next, > "go/$next.html"
