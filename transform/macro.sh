06d01f59299e4e4e9c3f0ec65cb0768aa26ec5ef
mumenrider <brian.aquinde@outlook.com>
2018-03-16 12:17:45 +0000

    - fixed rebuild

--- transform/macro.sh
+++ transform/macro.sh
@@ -18,5 +18,6 @@ done
 
 ./transform/page.sh > dist/next.html
 
+rm -r "go/assets/$next"
 mv "assets/liquefied/" "go/assets/$next"
 cat dist/next.html | sed s,assets/liquefied,assets/$next, > "go/$next.html"
