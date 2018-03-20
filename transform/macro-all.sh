7284aeb8550f3cc57cd379487b7ff3f74318cb64
mumenrider <brian.aquinde@outlook.com>
2018-03-20 07:32:32 +0000

    support multilang

---
+++ transform/macro-all.sh
@@ -0,0 +1,10 @@
+cat << 'EOF' | while read c; do echo "$c"; ./transform/macro.sh "$c"; done;
+chi/apply
+chi/rules
+chi/home
+chi/contact
+eng/apply
+eng/rules
+eng/home
+eng/contact
+EOF
--- transform/macro.sh
+++ transform/macro.sh
@@ -12,12 +12,14 @@ next="$1"
 
 rm src/*
 cp scenes/base.html src/base.html
-find scenes -name "$next.*" | while read file; do
+find scenes -path "*$next.*" | while read file; do
 	cp "$file" "src/next.${file##*.}"
 done
 
 ./transform/page.sh > dist/next.html
 
-rm -r "go/assets/$next"
+rm -rf "go/assets/$next"
+mkdir -p "$(dirname "go/assets/$next")"
 mv "assets/liquefied/" "go/assets/$next"
-cat dist/next.html | sed s,assets/liquefied,assets/$next, > "go/$next.html"
+mkdir -p "$(dirname "go/$next.html")"
+cat dist/next.html | sed s,assets/liquefied,/assets/$next, > "go/$next.html"
