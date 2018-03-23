e0a481881d936c4a7316f7f806bdb8dd656f454a
mumenrider <brian.aquinde@outlook.com>
2018-03-23 06:03:45 +0000

    + added taborder

--- transform/generator.js
+++ transform/generator.js
@@ -1,5 +1,11 @@
 var auto = require ('__auto')
 auto (() => {
+
+var Oo = require ('o-o-o-o-o') .Oo;
+var o = require ('o-o-o-o-o') .o;
+var oO = require ('o-o-o-o-o') .oO;
+var R = require ('ramda');
+
         var next = auto .frame ('next')
 	next .querySelector ('title') .remove ()
         Array .from (next .querySelectorAll ('#hint[for="anchor"]')) .forEach (x => {
@@ -14,5 +20,22 @@ auto (() => {
         Array .from (next .querySelectorAll ('#hint[for="select"]')) .forEach (x => {
                 x .outerHTML = auto .select_ify (x);
         })
+	Oo (Array .from (next .querySelectorAll ("[href]:not([tabindex='-1']),"
+			+ "area[href]:not([tabindex='-1']),"
+			+ "input:not([disabled]):not([tabindex='-1']),"
+			+ "select:not([disabled]):not([tabindex='-1']),"
+			+ "textarea:not([disabled]):not([tabindex='-1']),"
+			+ "button:not([disabled]):not([tabindex='-1']),"
+			+ "iframe:not([tabindex='-1']),"
+			+ "[tabindex]:not([tabindex='-1']),"
+			+ "[contentEditable=true]:not([tabindex='-1'])"
+		)),
+		o (R .reverse),
+		o (R .toPairs),
+		oO (R .forEach (x => {
+			var afters = x [0]
+			var node = x [1]
+			node .setAttribute ('tabindex', (+ afters + 1))
+		})))
         return next
 })
